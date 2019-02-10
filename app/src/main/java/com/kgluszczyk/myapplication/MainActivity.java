51ef8184d5c7328fba45d19d467a180131af94d9
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2019-02-10 20:38:13 +0100

    View reference

--- app/src/main/java/com/kgluszczyk/myapplication/MainActivity.java
+++ app/src/main/java/com/kgluszczyk/myapplication/MainActivity.java
@@ -5,12 +5,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tytuł).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Toast.makeText(MainActivity.this, "Klinkąłem na tytuł: " + ((TextView)v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
