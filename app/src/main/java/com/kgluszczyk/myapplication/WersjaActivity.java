0be5b4bc82a2e45c8db6c9ebeeb71fd86abb272a
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2019-02-10 22:02:38 +0100

    Add toolbar

--- app/src/main/java/com/kgluszczyk/myapplication/WersjaActivity.java
+++ app/src/main/java/com/kgluszczyk/myapplication/WersjaActivity.java
@@ -1,7 +1,12 @@
 package com.kgluszczyk.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class WersjaActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class WersjaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wersja);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else if (item.getItemId() == R.id.info) {
+            Toast.makeText(WersjaActivity.this, "Kurs programowania Android", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_info, menu);
+        return true;
     }
 }
