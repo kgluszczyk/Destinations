cf9f2b93c639c416b09ba8fed307dc3594f2d119
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2019-02-10 21:20:31 +0100

    Add new activity

--- app/src/main/java/com/kgluszczyk/myapplication/MainActivity.java
+++ app/src/main/java/com/kgluszczyk/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kgluszczyk.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -20,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(final View v) {
                 Toast.makeText(MainActivity.this, "Klinkąłem na tytuł: " + ((TextView)v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.wersja).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Intent intent = new Intent(MainActivity.this, WersjaActivity.class);
+                startActivity(intent);
             }
         });
     }
