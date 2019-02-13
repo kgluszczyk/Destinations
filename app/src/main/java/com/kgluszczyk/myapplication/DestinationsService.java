bfcc912d5e6f57ab9723b99758cb54513d0948d7
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2019-02-13 20:18:46 +0100

    Cleanup

--- app/src/main/java/com/kgluszczyk/myapplication/DestinationsService.java
+++ app/src/main/java/com/kgluszczyk/myapplication/DestinationsService.java
@@ -2,13 +2,9 @@ package com.kgluszczyk.myapplication;
 
 import io.reactivex.Single;
 import java.util.List;
-import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface DestinationsService {
-    @GET("fake-server/destinations")
-    Call<List<Destination>> getDestinationsCall();
-
     @GET("fake-server/destinations")
     Single<List<Destination>> getDestinationsSingle();
 }
